a36fe08e5641407f338e78081ceda8beae9f3187
Amal Babu <amalbabu1200@gmail.com>
2024-08-19 14:33:12 +0530

    home page alert

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,4 +1,4 @@
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const Home = () => {
 	const navigate = useNavigate();
@@ -65,6 +65,34 @@ const Home = () => {
 						>
 							Calculate CGPA
 						</button>
+					</div>
+
+					{/* home page alert  */}
+					<div className="mt-3">
+						<div
+							role="alert"
+							className="alert"
+						>
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								fill="none"
+								viewBox="0 0 24 24"
+								className="stroke-info h-6 w-6 shrink-0"
+							>
+								<path
+									strokeLinecap="round"
+									strokeLinejoin="round"
+									strokeWidth="2"
+									d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+								></path>
+							</svg>
+							<span>
+								<Link className="link-primary underline" to={"https://cgpa-sgpa--beta-4vi7tum2.web.app/"}>
+									4<sup>th</sup>sem SGPA and overall CGPA updated in beta
+									channel
+								</Link>
+							</span>
+						</div>
 					</div>
 				</div>
 			</div>
