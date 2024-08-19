5c3c56c33235db98aa86c7d3952eddc9b7f6a37a
Amal Babu <amalbabu1200@gmail.com>
2024-08-19 19:17:32 +0530

    priduction fix added beta link

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,4 +1,4 @@
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const Home = () => {
 	const navigate = useNavigate();
@@ -54,6 +54,28 @@ const Home = () => {
 						>
 							Calculate CGPA
 						</button>
+					</div>
+					{/* home page alert  */}
+					<div
+						role="alert"
+						className="alert"
+					>
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							fill="none"
+							viewBox="0 0 24 24"
+							className="stroke-info h-6 w-6 shrink-0"
+						>
+							<path
+								strokeLinecap="round"
+								strokeLinejoin="round"
+								strokeWidth="2"
+								d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+							></path>
+						</svg>
+						<span className="link-primary underline">
+							<Link to={"https://cgpa-sgpa--beta-4vi7tum2.web.app/"}>4<sup>th</sup>SEM SGPA & CGPA Live ON BETA</Link>
+						</span>
 					</div>
 				</div>
 			</div>
