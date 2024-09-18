936a475a10cd38ba269c40bc6ddc996b0c91225d
Amal Babu <amalbabu1200@gmail.com>
2024-09-18 18:28:01 +0530

    lices update style changes

--- src/components/header/Navbar.jsx
+++ src/components/header/Navbar.jsx
@@ -63,7 +63,7 @@ const Navbar = () => {
 								<a onClick={onClickSemThree}>SEM III SGPA</a>
 							</li>
 							<li>
-								<a onClick={onClickSemFour}>SEM III SGPA</a>
+								<a onClick={onClickSemFour}>SEM IV SGPA</a>
 							</li>
 							<li>
 								<a onClick={onClickCGPA}>CGPA</a>
@@ -72,6 +72,14 @@ const Navbar = () => {
 								<a onClick={onClickPrivacyPolicy}>
 									Terms and Conditions & Privacy Policy
 								</a>
+							</li>
+							<li>
+								<Link
+									to="https://github.com/amal-babu-git/cgpa-mca-vtu-2022-24/blob/production/LICENSE"
+									target="_blank"
+								>
+									License
+								</Link>
 							</li>
 						</ul>
 					</div>
@@ -82,13 +90,9 @@ const Navbar = () => {
 						className="btn btn-ghost normal-case text-xl "
 					>
 						<h1>CGPA Calculator MCA 2022-24</h1>
-
-						{/* <h1 className="font-bold text-transparent bg-clip-text bg-gradient-to-r  from-red-600 via-orange-600 via-yellow-600 via-green-800 via-blue-800 via-indigo-800 to-violet-800">
-							CGPA Calculator MCA 2022-24
-						</h1> */}
 					</Link>
 					<Link
-						className="text-primary font-semibold   npm install -g firebase-tools "
+						className="text-primary font-semibold"
 						to={"https://github.com/amal-babu-git"}
 						target="_blank"
 					>
@@ -98,41 +102,7 @@ const Navbar = () => {
 					</Link>
 				</div>
 				<div className="navbar-end">
-					{/* <button className="btn btn-ghost btn-circle">
-						<svg
-							xmlns="http://www.w3.org/2000/svg"
-							className="h-5 w-5"
-							fill="none"
-							viewBox="0 0 24 24"
-							stroke="currentColor"
-						>
-							<path
-								strokeLinecap="round"
-								strokeLinejoin="round"
-								strokeWidth="2"
-								d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-							/>
-						</svg>
-					</button>
-					<button className="btn btn-ghost btn-circle">
-						<div className="indicator">
-							<svg
-								xmlns="http://www.w3.org/2000/svg"
-								className="h-5 w-5"
-								fill="none"
-								viewBox="0 0 24 24"
-								stroke="currentColor"
-							>
-								<path
-									strokeLinecap="round"
-									strokeLinejoin="round"
-									strokeWidth="2"
-									d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
-								/>
-							</svg>
-							<span className="badge badge-xs badge-primary indicator-item"></span>
-						</div>
-					</button> */}
+					{/* Additional buttons can be added here */}
 				</div>
 			</div>
 		</>
